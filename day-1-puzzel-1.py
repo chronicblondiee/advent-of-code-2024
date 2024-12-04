d21a77052efd208bee0b6511ff5ab5a20d563396
Aodhan Brown <chronicblondiee@gmail.com>
2024-12-04 00:12:46 +0000

    day1 p1 done

--- day-1-puzzel-1.py
+++ day-1-puzzel-1.py
@@ -1,7 +1,7 @@
 # imports
 import polars as pl
 
-# Replace multi-byte separator with a standard space before reading
+# Replace multi-byte separator with single byte seperator before reading
 def preprocess_file(file_path, original_separator='   ', new_separator=','):
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
@@ -16,21 +16,20 @@ def preprocess_file(file_path, original_separator='   ', new_separator=','):
 # main()
 def main ():
     preprocess_file("inputs/day-1/day-1-input-data")
-
+    total_distance = 0
     file_path = "inputs/day-1/day-1-input-data-processed"
     # load data
-    df = pl.scan_csv(file_path, separator=",", encoding="utf8").collect()
+    df = pl.scan_csv(file_path, separator=",", encoding="utf8", has_header=False).collect()
+    # label columns
     df.columns = ["col1", "col2"]
+    # sort
     col1_series = df.get_column("col1").sort()
     col2_series = df.get_column("col2").sort()
-
-    print(col1_series)
-
-    #df = df.sort(["col1", "col2"])
-    #df = df.sort(["col2"])
-    #print(df.select(sorted(df.columns)))
-
-
+    # remove dataframe reference
+    df = None
+    # compute the differece sum
+    total_distance = sum(abs(d1 - d2) for d1, d2 in zip(col1_series, col2_series))
+    print(total_distance)
 
 if __name__ == "__main__":
     main()
